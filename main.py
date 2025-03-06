aba54ce11522ba7f667d1e8598abd0aa6e911538
Utkarsh Akhouri <uakhouri2000@gmail.com>
2025-03-06 13:00:07 -0500

    Handled Port Error for Deployment 3

--- main.py
+++ main.py
@@ -117,4 +117,5 @@ async def search_and_summarize(
   return {"papers":summarized_papers}
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=8080)
